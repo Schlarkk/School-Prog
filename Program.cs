2331b48f5cb3f461c1684a19f64400d57138c066
Schlarkk <jt@seaofdreams.nl>
2026-05-12 09:44:37 +0200

    ddada

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Program
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            String input = Console.ReadLine();
+            int UserChoice = int.Parse(input);
+            switch (UserChoice)
+            {
+                case 1:
+                    Console.WriteLine("Creating Student");
+                    break;
+                
+                case 2:
+                    Console.WriteLine("Creating Teacher");
+                    break;
+
+                case 3:
+                    Console.WriteLine("Creating Lesson");
+                    break;
+
+                default:
+                    Console.WriteLine("NUH UH");
+                    break;
+                
+            }
+
+            Console.WriteLine("wsg");
+        }
+    }
+}
